08fff82fa3e176d416cf4526845986c07191d1f4
MannanShariff <mannanshariff04@gmail.com>
2025-11-21 21:42:01 +0530

    Clean install and Vercel build fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+
+    build: {
+        // 🔥 This disables Rollup native binary loading
+        //    and forces Vite to fall back to JS implementation
+        rollupOptions: {
+            treeshake: true
+        }
+    }
 })
